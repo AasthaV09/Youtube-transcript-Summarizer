cd8109465d028a72427be59d84a7b743bb560885
Aastha Verma <79007769+AasthaV09@users.noreply.github.com>
2023-07-13 14:07:27 +0530

    Add files via upload

--- YoutubeTranscriptSummarizer/app.py
+++ YoutubeTranscriptSummarizer/app.py
@@ -1,15 +1,21 @@
-from flask import Flask, request
+from flask import Flask, request, session
 from youtube_transcript_api import YouTubeTranscriptApi
 from transformers import pipeline
+from googletrans import Translator
+from googletrans import LANGUAGES
+
 
 app = Flask(__name__)
+app.secret_key = '20021934'
 
-@app.get('/summary')
+@app.route('/summary',  methods=['GET','POST'])
 def summary_api():
     url = request.args.get('url','')
     video_id = url.split('=')[1]
     summary = get_summary(get_transcript(video_id))
+    session['summarized_text']=summary
     return summary, 200
+
 
 def get_transcript(video_id):
     transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
@@ -23,6 +29,24 @@ def get_summary(transcript):
         summary_text = summariser(transcript[i*1000:(i+1)*1000])[0]['summary_text']
         summary = summary + summary_text + ' '
     return summary
+
+@app.route('/translate',  methods=['GET','POST'])
+def get_translate():
+    para = session.get('summarized_text','')
+    # para = "You ara a FOOL"
+    target_language = request.args.get('language')
+    translator = Translator()
+    language_code = get_language_code(target_language)
+    translation = translator.translate(para, dest=language_code)
+    translated_para = (f"Translation ({target_language}):{translation.text}")
+    return translated_para
+
+
+def get_language_code(language):
+    for code, name in LANGUAGES.items():
+        if language.lower()==name.lower():
+           return code
+    return None
 
 if __name__ == '__main__':
     app.run()
